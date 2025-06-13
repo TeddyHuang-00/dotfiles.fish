2f53f45667a02812fb0dca5c9187580990404593
Nan Huang <teddyhuangnan@gmail.com>
2025-06-13 12:14:50 -0500

    Only init carapace and starship if present

--- config/config.fish
+++ config/config.fish
@@ -13,15 +13,19 @@ end
 # Commands to run in interactive sessions can go here
 if status is-interactive
     # Completion engine
-    carapace _carapace | source
+    if type -q caparace
+        carapace init fish | source
+    end
 
     # Prompt
-    function starship_transient_prompt_func
-        starship module character
+    if type -q starship
+        function starship_transient_prompt_func
+            starship module character
+        end
+        function starship_transient_rprompt_func
+            starship module time
+        end
+        starship init fish | source
+        enable_transience
     end
-    function starship_transient_rprompt_func
-        starship module time
-    end
-    starship init fish | source
-    enable_transience
 end
