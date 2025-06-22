38f97d3a60fd0ebdae79ab725348520f92e102d5
Nan Huang <teddyhuangnan@gmail.com>
2025-06-22 17:37:09 -0500

    Get the plugins before installing fisher

--- script/fish.fish
+++ script/fish.fish
@@ -1,4 +1,5 @@
 # Install plugins
+set -l plugins (cat (path resolve ~/.config/fish/fish_plugins))
 if type -q fisher
     echo "Using system-wide fisher installation"
 else
@@ -6,7 +7,7 @@ else
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 end
 echo "Installing plugins"
-cat (path resolve ~/.config/fish/fish_plugins) | fisher install
+echo $plugins | fisher install
 
 # Set theme
 fish_config theme save "Catppuccin Mocha"
