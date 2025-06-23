4d1a5cebde3403a241c3f9140fb30b160bd0cec8
Nan Huang <teddyhuangnan@gmail.com>
2025-06-23 15:16:22 -0500

    Enable continuum plugin to automatically save and restore the tmux session.

--- script/tmux.fish
+++ script/tmux.fish
@@ -6,7 +6,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 set -l fish_path (which fish)
 set -l config_path (path resolve ~/.config/tmux/tmux.conf)
 set -l config_content "$(cat $config_path)"
-string replace -a __FISH_SHELL_PATH__ "$fish_path" $config_content > $config_path
+string replace -a __FISH_SHELL_PATH__ "$fish_path" $config_content >$config_path
 
 # Finish the install in tmux
 set -gxa DOT_FILE_CAVEATS "Please finish the install in tmux by running <Ctrl-a> + I"
