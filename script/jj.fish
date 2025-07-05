bdc04cd2e882b60ae3a11bf02d0d8e778a9d6639
Nan Huang <teddyhuangnan@gmail.com>
2025-07-05 17:20:35 -0500

    Check for delta installation in jujutsu as well

--- script/jj.fish
+++ script/jj.fish
@@ -1,2 +1,7 @@
+# Check if delta is installed
+if not type -q delta
+    set -gxa DOT_FILE_CAVEATS "delta is not installed, please install it to make sure the jujutsu diff works"
+end
+
 # Export dependency
 set -gxa DOT_FILE_DEPS jj delta
