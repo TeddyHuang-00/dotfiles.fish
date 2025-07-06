ebe6fe8ce825c6200cd4c14dba073a830ce002ba
Nan Huang <teddyhuangnan@gmail.com>
2025-07-05 22:29:51 -0500

    Add configuration for jjui

---
+++ script/jjui.fish
@@ -0,0 +1,7 @@
+# Check if delta is installed
+if not type -q delta
+    set -gxa DOT_FILE_CAVEATS "delta is not installed, please install it to make sure the custom diff works"
+end
+
+# Export dependency
+set -gxa DOT_FILE_DEPS jjui delta
