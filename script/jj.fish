8ad7cab54bdfa15f2e1d13335b4f541db20abc79
Nan Huang <teddyhuangnan@gmail.com>
2026-03-23 19:55:32 -0500

    Use diffnav instead of delta for jj diff

--- script/jj.fish
+++ script/jj.fish
@@ -1,7 +1,11 @@
-# Check if delta is installed
+# Check if key dependencies are installed
 if not type -q delta
     set -gxa DOT_FILE_CAVEATS "delta is not installed, please install it to make sure the jujutsu diff works"
 end
 
+if not type -q diffnav
+    set -gxa DOT_FILE_CAVEATS "diffnav is not installed, please install it to make sure the jujutsu diff works"
+end
+
 # Export dependency
-set -gxa DOT_FILE_DEPS jj delta
+set -gxa DOT_FILE_DEPS jj delta diffnav
